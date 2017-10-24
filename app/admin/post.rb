a2bf9bec4609bad9b15fa14425d3b7cfa4f895c7
Oleksandr <targonsky97@gmail.com>
2017-10-24 15:22:48 +0300

    fix authors names select

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,11 +1,19 @@
 ActiveAdmin.register Post do
 		permit_params :title, :body, :image, :author_id, :author
 
+  index do
+    selectable_column
+    column :title
+    column :body
+    column :author
+    actions
+  end
+
 		show do |t|
 			attributes_table do
 				row :title
 				row :body
-				row :author 
+				row :author
 				row :image do
 					post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "No photo yet")
 				end
@@ -13,12 +21,13 @@ ActiveAdmin.register Post do
 		end
 
 		form :html => { :enctype => "multipart/form-data" } do |f|
-			f.inputs do
+			f.inputs 'Post Editing' do
 				f.input :title
 				f.input :body
-				f.input :author
+				f.input :author 
 				f.input :image, hint: f.post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "Upload JPG/PNG/GIF image")
 			end
 			f.actions
 		end
 end
+
