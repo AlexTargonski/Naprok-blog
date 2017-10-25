9ff8447e55c2a29a7b384690ddd6c855fe032963
Oleksandr <targonsky97@gmail.com>
2017-10-25 21:44:09 +0300

    pg

--- spec/models/author_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Author do
-	it 'New author' do
-		@author = FactoryGirl.build(:author)
-		expect(@author.name).to eq("User")
-    expect(@author.position).to eq("example")
-  end
-end
